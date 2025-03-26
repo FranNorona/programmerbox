182c27686a0fc787c00c13b22f31a19d9846a61e
franconorona95 <franconorona95@gmail.com>
2025-03-26 14:39:45 +0000

    Testing

--- src/components/navbar/NavListDrawer.jsx
+++ src/components/navbar/NavListDrawer.jsx
@@ -6,8 +6,8 @@ import LogoutButton from "../logoutButton/LogoutButton";
 
 const NavListDrawer = ({ navLinks }) => {
   return (
-    <Box className="lex flex-col items-center !w-42 text-[white] bg-emerald-400 min-h-screen" sx={{height: "100vh"}}>
-      <nav className="flex flex-col items-center justify-between flex-1 !p-2">
+    <Box className="flex flex-col items-center !w-42 text-[white] bg-emerald-400" sx={{height: "100vh"}}>
+      <nav className="flex flex-col items-center justify-between h-[100vh] !p-2">
         <List>
           {navLinks.map((item) => (
             <ListItem disablePadding key={item.title}>
